213cc532a351c032487bf2f3c37ca8ae0087d91e
Adiesha Liyanage <adieshal@lseg.com>
2018-01-28 22:36:16 +0530

    Refactored code

--- src/main/java/com/adiesha/swing/App.java
+++ src/main/java/com/adiesha/swing/App.java
@@ -5,49 +5,33 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
-import static java.lang.System.exit;
-
 
 public class App
 {
-    public static void main( String[] args ) {
-        byte signedByte = -1;
-        int unsignedByte =  signedByte & 0xff;
+    public static void main( String[] args ) throws IOException {
 
-        System.out.println("Signed: " + signedByte + " Unsigned: " + unsignedByte);
-        int as = 132;
-        byte as2 = (byte) as;
-        System.out.println(as2);
         System.out.println( "Hello World!" );
-        System.out.println(Integer.toString(-122,16));
-        SHA256CheckSumCalculator sha256CheckSumCalculator = new SHA256CheckSumCalculator();
-        try {
-            sha256CheckSumCalculator.run();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
         JFrame f = new JFrame();
-
-
-        final JTextField tf = new JTextField();
-        tf.setBounds(50,50,150,20);
         JButton jb = new JButton("Click Here");
         jb.setBounds(50,100,95,30);
         jb.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                tf.setVisible(true);
-                tf.setText("Button clicked");
+                CheckSumCalculator checkSumCalculator = CheckSumCalculator.getInstance();
+                byte[] result = null;
+                try {
+                    result = checkSumCalculator.calculate(1,"E:\\Apps\\datagrip-2017.3.4.exe");
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                CheckSumCalculator.parseByteToHexFormat(result);
             }
         });
 
         f.add(jb);
-        f.add(tf);
         f.setSize(400,400);
         f.setLayout(null);
         f.setVisible(true);
-        tf.setVisible(false);
 
     }
 }
---
+++ src/main/java/com/adiesha/swing/CheckSumCalculator.java
@@ -0,0 +1,82 @@
+package com.adiesha.swing;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.System.exit;
+
+public class CheckSumCalculator {
+
+    private static CheckSumCalculator privateInstance = null;
+    private static Map<Integer, String> mapAlgoIDtoName = null;
+
+    protected CheckSumCalculator() {
+        mapAlgoIDtoName = new HashMap<Integer, String>();
+        mapAlgoIDtoName.put(1, "SHA-256");
+        mapAlgoIDtoName.put(2, "SHA-512");
+        mapAlgoIDtoName.put(3, "MD5");
+    }
+
+    public static synchronized CheckSumCalculator getInstance() {
+        if (privateInstance == null) {
+            privateInstance = new CheckSumCalculator();
+        }
+        return privateInstance;
+    }
+
+    public byte[] calculate(int algoID, String filePath) throws IOException {
+        MessageDigest md = getHashAlgo(algoID);
+        FileInputStream fileInputStream = getFileInputStream(filePath);
+        byte[] dataBytes = new byte[1024];
+        int nread = 0;
+        while ((nread = fileInputStream.read(dataBytes)) != -1) {
+            md.update(dataBytes, 0, nread);
+        }
+
+        byte[] mdbytes = md.digest();
+        return mdbytes;
+    }
+
+    public MessageDigest getHashAlgo(int algoID) {
+        if (!mapAlgoIDtoName.containsKey(algoID)) {
+            System.out.println("Choosing default algorithm SHA-256");
+            algoID = 1;
+        }
+        try {
+            return MessageDigest.getInstance(mapAlgoIDtoName.get(algoID));
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("No such algorithm exist");
+            e.printStackTrace();
+            exit(-1);
+        }
+        return null;
+    }
+
+    public FileInputStream getFileInputStream(String filePath) {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Error: cannot read the file");
+            exit(-1);
+        }
+        return fileInputStream;
+    }
+
+    public static void parseByteToHexFormat(byte[] input) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < input.length; i++) {
+            System.out.println(input[i]);
+            sb.append(Integer.toString((input[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        System.out.println("Hex Format: " + sb.toString());
+        System.out.println("Hex Format: " + DatatypeConverter.printHexBinary(input));
+    }
+}
